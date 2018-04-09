720a5f1c9a12ec5102c0711c3394fc502e94d1e1
Robb Leitao <robertoleitaov@gmail.com>
2018-04-09 14:14:42 -0300

    giladas frontend.. todo mas lindo

--- public/js/main.js
+++ public/js/main.js
@@ -144,29 +144,6 @@
 		// }, { offset: '90%'} );
 	};
 
-
-	var dropdown = function() {
-
-		$('.has-dropdown').mouseenter(function(){
-
-			var $this = $(this);
-			$this
-				.find('.dropdown')
-				.css('display', 'block')
-				.addClass('animated-fast fadeInUpMenu');
-
-		}).mouseleave(function(){
-			var $this = $(this);
-
-			$this
-				.find('.dropdown')
-				.css('display', 'none')
-				.removeClass('animated-fast fadeInUpMenu');
-		});
-
-	};
-
-
 	var owlCarousel = function(){
 		
 		var owl = $('.owl-carousel-carousel');
@@ -277,7 +254,6 @@
 		offcanvasMenu();
 		burgerMenu();
 		contentWayPoint();
-		dropdown();
 		owlCarousel();
 		goToTop();
 		loaderPage();
--- public/src/views/home/homeCtrl.js
+++ public/src/views/home/homeCtrl.js
@@ -5,6 +5,15 @@ angular.module("app")
             var vm = this;
             vm.saludo = sessionStorage.UserName;
 
+
+            vm.toggleAction = function(dropped){
+                vm.actionsDropped = dropped;
+            }
+
+            vm.isAdmin = true; //true lo muestra -> false no lo muestra
+            //Esto lo saco de algun lado
+
+
             vm.logout = function(){
                 sessionStorage.clear();
                 $window.location.href = '#!/login';
